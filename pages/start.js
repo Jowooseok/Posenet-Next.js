4443641974edf502e7ba5e8670883e3576858353
Jowooseok <whdntjrsla12@naver.com>
2019-10-24 01:05:26 +0900

    운동 준비단계 까지 완료  해야할 작업: 운동준비상태에서 시작하면 운동화면 나오면서 Go하기

--- pages/start.js
+++ pages/start.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Link from 'next/link';
-import { Steps, Button, message, Card, Row, Col, Drawer } from 'antd';
+import { Steps, Button, message, Card, Row, Col, Drawer, Form, Input, Divider } from 'antd';
 
 
 const { Step } = Steps;
@@ -16,7 +16,7 @@ const steps = [
         content: 'Second-content',
     },
     {
-        title: '운동시작하기',
+        title: '운동준비',
         content: 'Last-content',
     },
 ];
@@ -25,8 +25,19 @@ const steps = [
 const start = () => {
 
     const [current, setCurrent] = useState(0);
-    const [visible, setVisible] = useState(false);
-    const [exercise, setExercise] = useState('');
+    const [deadliftVisible, setDeadliftVisible] = useState(false);
+    const [pushupVisible, setPushupVisible] = useState(false);
+    const [squatVisible, setSquatVisible] = useState(false);
+    const [situpVisible, setSitupVisible] = useState(false);
+    const [deadliftNumber, setDeadliftNumber] = useState('');
+    const [pushupNumber , setPushupNumber] = useState('');
+    const [squatNumber, setSquatNumber] = useState('');
+    const [situpNumber, setSitupNumber] = useState('');
+    const [deadliftSets, setDeadliftSets] = useState('');
+    const [pushupSets , setPushupSets] = useState('');
+    const [squatSets, setSquatSets] = useState('');
+    const [situpSets, setSitupSets] = useState('');
+
 
     const next = () => {
         setCurrent(current + 1);
@@ -35,48 +46,106 @@ const start = () => {
         setCurrent(current - 1);
     }
 
-    const showDrawer = (e) => {
-        setVisible(true); 
+    const showDeadliftDrawer = () => {
+        setDeadliftVisible(true);
     }
 
-    const onClose = () => {
-        setVisible(false);
+    const onCloseDeadlift = () => {
+        setDeadliftVisible(false);
     }
 
-    const deadliftState = () =>{
-        setExercise("데드리프트")
+    const showPushupDrawer = () => {
+        setPushupVisible(true);
     }
 
-    const pushupState = () =>{
-        setExercise("팔굽혀펴기")
+    const onClosePushup = () => {
+        setPushupVisible(false);
+    }
+    const showSquatDrawer = () => {
+        setSquatVisible(true);
     }
 
-    const squatState = () =>{
-        setExercise("스쿼트")
+    const onCloseSquat = () => {
+        setSquatVisible(false);
+    }
+    const showSitupDrawer = () => {
+        setSitupVisible(true);
     }
 
-    const situpState = () =>{
-        setExercise("윗몸일으켜기")
+    const onCloseSitup = () => {
+        setSitupVisible(false);
     }
 
+    const onDeadliftSubmit = (e) =>{
+        e.preventDefault
+        onCloseDeadlift()
+    }
+
+    const onChangeDeadliftNumber = (e) =>{
+        setDeadliftNumber(e.target.value)
+    }
+
+    const onChangeDeadliftSets = (e) =>{
+        setDeadliftSets(e.target.value)
+    }
+
+    const onPushupSubmit = (e) =>{
+        e.preventDefault
+        onClosePushup()
+    }
+
+    const onChangePushupNumber = (e) =>{
+        setPushupNumber(e.target.value)
+    }
+
+    const onChangePushupSets = (e) =>{
+        setPushupSets(e.target.value)
+    }
+
+    const onSitupSubmit = (e) =>{
+        e.preventDefault
+        onCloseSitup()
+    }
+
+    const onChangeSitupNumber = (e) =>{
+        setSitupNumber(e.target.value)
+    }
+
+    const onChangeSitupSets = (e) =>{
+        setSitupSets(e.target.value)
+    }
+
+    const onSquatSubmit = (e) =>{
+        e.preventDefault
+        onCloseSquat()
+    }
+
+    const onChangeSquatNumber = (e) =>{
+        setSquatNumber(e.target.value)
+    }
+
+    const onChangeSquatSets = (e) =>{
+        setSquatSets(e.target.value)
+    }
+    
     const contents = () => {
         if (current == 0) {
             return (
                 <div style={{ background: '#ECECEC', padding: '30px' }}>
                     <Row gutter={16}>
                         <Col span={8}>
-                            <Card title="Card title" bordered={false}>
-                                Card content
+                            <Card title="날짜" bordered={false}>
+                                운동내역
                 </Card>
                         </Col>
                         <Col span={8}>
-                            <Card title="Card title" bordered={false}>
-                                Card content
+                            <Card title="날짜" bordered={false}>
+                                운동내역
                 </Card>
                         </Col>
                         <Col span={8}>
-                            <Card title="Card title" bordered={false}>
-                                Card content
+                            <Card title="날짜" bordered={false}>
+                                운동내역
                 </Card>
                         </Col>
                     </Row>
@@ -86,6 +155,36 @@ const start = () => {
         }
         else if (current == 1) {
             return (
+                <div>
+                    <Card size="small" title="운동설정현황" extra={<></>} style={{ width: 300, height: 520 , position:'absolute', right : 800, top : 200}}> {/* 나중에 right 수정 */}
+                    {
+                            <div>
+                                <div>
+                                    <h3>데드리프트</h3>
+                                    개수 : {deadliftNumber} <br />
+                                    세트 : {deadliftSets}
+                                    <Divider />
+                                </div>
+                                <div>
+                                    <h3>팔굽혀펴기</h3>
+                                    개수 : {pushupNumber} <br />
+                                    세트 : {pushupSets}
+                                    <Divider />
+                                </div>
+                                <div>
+                                    <h3>스쿼트</h3>
+                                    개수 : {squatNumber} <br />
+                                    세트 : {squatSets}
+                                    <Divider />
+                                </div>
+                                <div>
+                                    <h3>윗몸일으켜기</h3>
+                                    개수 : {situpNumber} <br />
+                                    세트 : {situpSets}
+                                </div>
+                            </div>
+                    }
+                </Card>
                 <div style={{
                     height: 780,
                     overflow: 'hidden',
@@ -95,72 +194,159 @@ const start = () => {
                     padding: 48,
                     textAlign: 'center',
                     background: '#fafafa',
-                  }}>
+                }}>
+                    
                     <Row gutter={15} style={{ marginBottom: 15 }}>
                         <Col span={12}>
-                            <Card title="데드리프트" bordered={true}
-                                cover={<img alt="deadlift" src="../static/deaflift.png" style={{ marginTop: 30, height: 200 }} />}
-                                onClick={showDrawer}
+                            <Card title="데드리프트" bordered={true} style={{cursor : "pointer"}}
+                                cover={<img alt="데드리프트" src="../static/deaflift.png" style={{ marginTop: 30, height: 200 }} />}
+                                onClick={showDeadliftDrawer}
                             >
 
                             </Card>
                         </Col>
                         <Col span={12}>
-                            <Card title="팔굽혀펴기" bordered={true}
-                                cover={<img alt="deadlift" src="../static/pushup.png" style={{ marginTop: 30, height: 200 }} />}
-                                onClick={showDrawer}
+                            <Card title="팔굽혀펴기" bordered={true} style={{cursor : "pointer"}}
+                                cover={<img alt="팔굽혀펴기" src="../static/pushup.png" style={{ marginTop: 30, height: 200 }} />}
+                                onClick={showPushupDrawer}
                             >
                             </Card>
                         </Col>
                     </Row>
                     <Row gutter={15} style={{ marginTop: 15 }}>
                         <Col span={12}>
-                            <Card title="스쿼트" bordered={true}
-                                cover={<img alt="deadlift" src="../static/squat.png" style={{ marginTop: 30, height: 200 }} />}
-                                onClick={showDrawer}
+                            <Card title="스쿼트" bordered={true} style={{cursor : "pointer"}}
+                                cover={<img alt="스쿼트" src="../static/squat.png" style={{ marginTop: 30, height: 200 }} />}
+                                onClick={showSquatDrawer}
                             >
                             </Card>
                         </Col>
                         <Col span={12}>
-                            <Card title="윗몸일으켜기" bordered={true}
-                                cover={<img alt="deadlift" src="../static/situp.jpg" style={{ marginTop: 30, height: 200 }} />}
-                                onClick={showDrawer}
+                            <Card title="윗몸일으켜기" bordered={true} style={{cursor : "pointer"}}
+                                cover={<img alt="윗몸일으켜기" src="../static/situp.jpg" style={{ marginTop: 30, height: 200 }} />}
+                                onClick={showSitupDrawer}
                             >
                             </Card>
                         </Col>
                     </Row>
 
                     <Drawer
-                        title={exercise}
+                        title={'데드리프트'}
                         placement="right"
                         closable={false}
-                        onClose={onClose}
-                        visible={visible}
-                        getContainer={false}
+                        onClose={onCloseDeadlift}
+                        visible={deadliftVisible}
+                        getContainer={true}
                         style={{ position: 'absolute' }}
                     >
-                        <p>Some contents...</p>
+                            <Form onSubmit = {onDeadliftSubmit}>
+                                <div>
+                                    개수 : <Input value={deadliftNumber} placeholder='0' onChange = {onChangeDeadliftNumber} required/>
+                                </div>
+                                <div style = {{marginTop : 10}}>
+                                    세트 : <Input value={deadliftSets} placeholder='0' onChange = {onChangeDeadliftSets} required/>
+                                </div>
+                                <Button type="primary" htmlType="submit" style={{marginTop : 15}}>등록</Button>
+                            </Form>
+                    </Drawer>
+                    <Drawer
+                        title={'팔굽혀펴기'}
+                        placement="right"
+                        closable={false}
+                        onClose={onClosePushup}
+                        visible={pushupVisible}
+                        getContainer={true}
+                        style={{ position: 'absolute' }}
+                    >
+                        <Form onSubmit = {onPushupSubmit}>
+                                <div>
+                                    개수 : <Input value={pushupNumber} placeholder='0' onChange = {onChangePushupNumber} required/>
+                                </div>
+                                <div style = {{marginTop : 10}}>
+                                    세트 : <Input value={pushupSets} placeholder='0' onChange = {onChangePushupSets} required/>
+                                </div>
+                                <Button type="primary" htmlType="submit" style={{marginTop : 15}}>등록</Button>
+                            </Form>
+                    </Drawer>
+                    <Drawer
+                        title={'스쿼트'}
+                        placement="right"
+                        closable={false}
+                        onClose={onCloseSquat}
+                        visible={squatVisible}
+                        getContainer={true}
+                        style={{ position: 'absolute' }}
+                    >
+                         <Form onSubmit = {onSquatSubmit}>
+                                <div>
+                                    개수 : <Input value={squatNumber} placeholder='0' onChange = {onChangeSquatNumber} required/>
+                                </div>
+                                <div style = {{marginTop : 10}}>
+                                    세트 : <Input value={squatSets} placeholder='0' onChange = {onChangeSquatSets} required/>
+                                </div>
+                                <Button type="primary" htmlType="submit" style={{marginTop : 15}}>등록</Button>
+                            </Form>
+                    </Drawer>
+                    <Drawer
+                        title={'윗몸일으켜기'}
+                        placement="right"
+                        closable={false}
+                        onClose={onCloseSitup}
+                        visible={situpVisible}
+                        getContainer={true}
+                        style={{ position: 'absolute' }}
+                    >
+                        <Form onSubmit = {onSitupSubmit}>
+                                <div>
+                                    개수 : <Input value={situpNumber} placeholder='0' onChange = {onChangeSitupNumber} required/>
+                                </div>
+                                <div style = {{marginTop : 10}}>
+                                    세트 : <Input value={situpSets} placeholder='0' onChange = {onChangeSitupSets} required/>
+                                </div>
+                                <Button type="primary" htmlType="submit" style={{marginTop : 15}}>등록</Button>
+                            </Form>
                     </Drawer>
 
+                </div>
                 </div>
             )
         } else {
             return (
                 <div style={{ background: '#ECECEC', padding: '30px' }}>
                     <Row gutter={16}>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={false}>
-                                Card content
-                </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={false}>
-                                Card content
-                </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={false}>
-                                Card content
+                        <Col span={24}>
+                        <Card size="small" title="운동설정현황" extra={<></>} style={{ }}> {/* 나중에 right 수정 */}
+                    {
+                            <div>
+                                <div style={{marginBottom : 40}}>
+                                    <h3>데드리프트</h3>
+                                    <img alt="데드리프트이미지" src="../static/deaflift.png" style={{ height: 75 , right : 15   ,   position:'absolute'}} />
+                                    개수 : {deadliftNumber} <br />
+                                    세트 : {deadliftSets}
+                                    <Divider />
+                                </div>
+                                <div style={{marginBottom : 40}}>
+                                    <h3>팔굽혀펴기</h3>
+                                    <img alt="팔굽혀펴기이미지" src="../static/pushup.png" style={{ height: 75 , right : 15   ,   position:'absolute'}} />
+                                    개수 : {pushupNumber} <br />
+                                    세트 : {pushupSets}
+                                    <Divider />
+                                </div>
+                                <div style={{marginBottom : 40}}>
+                                    <h3>스쿼트</h3>
+                                    <img alt="스쿼트이미지" src="../static/squat.png" style={{ height: 75 , right : 15   ,   position:'absolute'}} />
+                                    개수 : {squatNumber} <br />
+                                    세트 : {squatSets}
+                                    <Divider />
+                                </div>
+                                <div style={{marginBottom : 30}}>
+                                    <h3>윗몸일으켜기</h3>
+                                    <img alt="윗몸일으켜기이미지" src="../static/situp.jpg" style={{ height: 75 , right : 15   ,   position:'absolute'}} />
+                                    개수 : {situpNumber} <br />
+                                    세트 : {situpSets}
+                                </div>
+                            </div>
+                    }
                 </Card>
                         </Col>
                     </Row>
@@ -171,7 +357,7 @@ const start = () => {
     }
 
     return (
-        <div style={{ marginTop: 30 }}>
+        <div style={{ marginTop: 30  }}>
             <Steps current={current} style={{ marginBottom: 20 }}>
                 {steps.map(item => (
                     <Step key={item.title} title={item.title} />
